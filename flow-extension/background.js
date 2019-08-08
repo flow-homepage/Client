88e56610a7f601ab568433ac2a7822bc89c742a7
cbrand125 <cbrand@talentpath.com>
2019-08-08 10:39:55 -0700

    finish extension

--- flow-extension/background.js
+++ flow-extension/background.js
@@ -1,11 +1,21 @@
+/**
+ * This file is the background script for the Chrome extension.
+ * Basically, Chrome extensions run their own hidden pages
+ * and they have background scripts driving them.
+ */
+
+// Add a listener on the installed event for this extension
 chrome.runtime.onInstalled.addListener(function() {
+  // When installed, save off some data in persistent storage
   chrome.storage.sync.set({ color: 'white' }, function() {
     console.log('The color is green.');
   });
 
+  // When installed, add remove all rules for this extension and then add our new ones
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
       {
+        // If we detect we are on our site, our extension becomes active
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
             pageUrl: {
@@ -20,7 +30,9 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
+// Add a listener on the message recieved event
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // Send back the sessions object containing all recently closed sessions to whomever sent the message
   chrome.sessions.getRecentlyClosed(sessions => {
     sendResponse(sessions);
   });
--- flow-extension/content.js
+++ flow-extension/content.js
@@ -1,6 +1,12 @@
+// In the manifest, this script is set to only be injected into pages that match our site
+
+// This will send a message to the background script
 chrome.runtime.sendMessage({ greeting: 'hello' }, function(sessions) {
+  // When the background script responds with the sessions,
+  // we will list out the recently closed tab/window names
   for (let i = 0; i < sessions.length; i++) {
     const page = sessions[i].tab || sessions[i].window;
+    document.getElementById('recentTabs').innerHTML = '';
     document.getElementById('recentTabs').innerHTML += page.title + '<br />';
   }
 });
