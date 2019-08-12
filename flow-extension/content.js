a4d126cec5dc63bad77779216a93d1fb7ff948b7
cbrand125 <cbrand@talentpath.com>
2019-08-12 08:37:50 -0700

    fix small bug

--- flow-extension/content.js
+++ flow-extension/content.js
@@ -4,9 +4,9 @@
 chrome.runtime.sendMessage({ greeting: 'hello' }, function(sessions) {
   // When the background script responds with the sessions,
   // we will list out the recently closed tab/window names
+  document.getElementById('recentTabs').innerHTML = '';
   for (let i = 0; i < sessions.length; i++) {
     const page = sessions[i].tab || sessions[i].window;
-    document.getElementById('recentTabs').innerHTML = '';
     document.getElementById('recentTabs').innerHTML += page.title + '<br />';
   }
 });
