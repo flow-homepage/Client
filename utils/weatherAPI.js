a79a37e8a183fb4825894dd234b9e06477297faf
Ivy <yunjilim@gmail.com>
2019-08-14 09:48:36 -0700

    weather display set for Los Angeles

--- utils/weatherAPI.js
+++ utils/weatherAPI.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const weatherDisplay = document.getElementById('weather');
+
 function getWeather() {
-  const weather = axios.get('http://localhost:5000/api/weather')
-  document.getElementById('weather').innerHTML = weather;
+  const weather = axios.get('http://localhost:5000/api/weather').then(data => JSON.parse(data));
+  weatherDisplay.innerHTML = weather;
 }
