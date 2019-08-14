c5b11f3e82669cd319f2505d62a59df5ff35596f
Ivy <yunjilim@gmail.com>
2019-08-14 08:41:51 -0700

    merging master

--- utils/weatherAPI.js
+++ utils/weatherAPI.js
@@ -1,5 +1,6 @@
+import axios from 'axios';
 
 function getWeather() {
-
+  const weather = axios.get('http://localhost:5000/api/weather')
   document.getElementById('weather').innerHTML = weather;
 }
