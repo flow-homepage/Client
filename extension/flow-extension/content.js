eb538c356d7ec28b41d138965c5c998a5de30672
cbrand125 <cbrand@talentpath.com>
2019-08-16 11:13:44 -0700

    add dynamically updating tabs

--- extension/flow-extension/content.js
+++ extension/flow-extension/content.js
@@ -4,6 +4,19 @@
 chrome.runtime.sendMessage({ recentlyClosed: 'true' }, function(sessions) {
   // When the background script responds with the sessions,
   // we will list out the recently closed tab/window names
+  updateTabWidget(sessions);
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // Send back the sessions object containing all recently closed sessions to whomever sent the message
+  if (request.newClosed === 'true') {
+    updateTabWidget(request.sessions);
+  }
+
+  return true;
+});
+
+function updateTabWidget(sessions) {
   document.getElementById('recentTabs').innerHTML = '';
   for (let i = 0; i < sessions.length; i++) {
     const page = sessions[i].tab || sessions[i].window;
@@ -14,4 +27,4 @@ chrome.runtime.sendMessage({ recentlyClosed: 'true' }, function(sessions) {
       page.title +
       `</a>`;
   }
-});
+}
