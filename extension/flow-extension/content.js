14c12e80912291e326fe348d839026b6cc4fd899
cbrand125 <cbrand@talentpath.com>
2019-08-19 09:52:15 -0700

    fix bug with tabs in windows

--- extension/flow-extension/content.js
+++ extension/flow-extension/content.js
@@ -3,7 +3,7 @@
 // This will send a message to the background script
 chrome.runtime.sendMessage({ recentlyClosed: 'true' }, function(sessions) {
   // When the background script responds with the sessions,
-  // we will list out the recently closed tab/window names
+  // we will list out the recently closed tab names
   updateTabWidget(sessions);
 });
 
@@ -16,15 +16,39 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   return true;
 });
 
+/**
+ * takes in an array of sessions and updates the tab widget on the page with them
+ * @param {[Session]} sessions chrome session objects
+ */
 function updateTabWidget(sessions) {
+  // remove "requires extension" message
   document.getElementById('recentTabs').innerHTML = '';
+
+  // loop over every session
   for (let i = 0; i < sessions.length; i++) {
-    const page = sessions[i].tab || sessions[i].window;
-    document.getElementById('recentTabs').innerHTML +=
-      `<a class="recentTab" title="${page.url}" href="${
-        page.url
-      }" target="_blank">` +
-      page.title +
-      `</a>`;
+    const { tab, window } = sessions[i];
+    if (tab) {
+      updateWidgetWithTab(tab);
+    } else {
+      // if the session was a window, loop over every tab in the window
+      for (let j = 0; j < window.tabs.length; j++) {
+        const windowTab = window.tabs[j];
+        updateWidgetWithTab(windowTab);
+      }
+    }
   }
+}
+
+/**
+ * uses a single tab object to create a single button for the widget
+ * @param {Tab} tab chrome tab object
+ */
+function updateWidgetWithTab(tab) {
+  document.getElementById(
+    'recentTabs'
+  ).innerHTML += `<a class="recentTab" title="${tab.url}" href="${
+    tab.url
+  }" target="_blank">
+    ${tab.title}
+    </a>`;
 }
