d4d009b4a1aa8963747b062d87c922e359c87747
Dean Sumitro <DSumitro@talentpath.com>
2019-08-27 09:05:50 -0700

    cleaned up file structure

--- utils/weatherAPI.js
+++ utils/weatherAPI.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const weatherDisplay = document.getElementById('weather');
+// const weatherDisplay = document.getElementById('weather');
 
-function getWeather() {
-  const weather = axios.get('http://localhost:5000/api/weather').then(data => JSON.parse(data));
-  weatherDisplay.innerHTML = weather;
+// function getWeather() {
+//   const weather = axios.get('http://localhost:5000/api/weather').then(data => JSON.parse(data));
+//   weatherDisplay.innerHTML = weather;
+// }
+async function getWeather() {
+  const weatherDisplay = document.getElementById('weather');
+  const weather = await axios.get('http://localhost:5000/api/weather');
+  weatherDisplay.innerHTML = `${weather.data.temperature}°F`;
 }
