5770e4af09e319cb380839f354e816866f942d18
Dean Sumitro <DSumitro@talentpath.com>
2019-09-09 10:29:42 -0700

    added photo credits still need styling

--- utils/backgroundAPI.js
+++ utils/backgroundAPI.js
@@ -1,7 +1,11 @@
 async function getBackground() {
   const htmlBackground = document.getElementById('page');
+  const photoCredits = document.getElementById('profileLink');
   const newImage = await axios.get('http://localhost:5000/api/background');
-  htmlBackground.style.background = `linear-gradient(rgba(36, 51, 91, 0.5), rgba(36, 51, 91, 0.5)), url('${newImage.data}')`;
+  htmlBackground.style.background = `linear-gradient(rgba(36, 51, 91, 0.5), rgba(36, 51, 91, 0.5)), url('${newImage.data.urls.regular}')`;
+  htmlBackground.style.backgroundSize = `cover`;
+  photoCredits.innerHTML = `${newImage.data.user.name}`;
+  photoCredits.href = `${newImage.data.user.links.html}`;
 }
 
 getBackground();
