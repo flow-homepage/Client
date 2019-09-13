87039277f1ce624d0227d6409745dbbb7b01ac82
Dean Sumitro <DSumitro@talentpath.com>
2019-09-13 10:36:21 -0700

    added utm source

--- utils/backgroundAPI.js
+++ utils/backgroundAPI.js
@@ -7,7 +7,7 @@ async function getBackground() {
   htmlBackground.style.background = `linear-gradient(rgba(36, 51, 91, 0.5), rgba(36, 51, 91, 0.5)), url('${newImage.data.urls.regular}')`;
   htmlBackground.style.backgroundSize = `cover`;
   photoCredits.innerHTML = `${newImage.data.user.name}`;
-  photoCredits.href = `${newImage.data.user.links.html}`;
+  photoCredits.href = `${newImage.data.user.links.html}?utm_source=flow_homepage&utm_medium=referral`;
 }
 
 getBackground();
