0fca1374a5ec9a6cdf69f8a2916023c338430c03
Dean Sumitro <DSumitro@talentpath.com>
2019-09-30 08:33:38 -0700

    added routing and component-ification

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import "./css/styles.css";
 import MomentTime from "./components/MomentTime";
 import Weather from "./components/Weather";
-import BrowserHistory from "./components/BrowserHistory";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Header from "./components/Header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 
 class App extends Component {
   constructor(props) {
@@ -37,27 +38,23 @@ class App extends Component {
   render() {
     const { lat, lng } = this.state;
     return (
-      <React.Fragment>
-        <header>
-          <section className="logoWithText">
-            <img src="/img/flowlogo.png" alt="" className="logoImg" />
-          </section>
+      <>
+        <Router>
+          <Header />
           <section>
             <MomentTime />
             <Weather lat={lat} lng={lng} />
           </section>
-        </header>
-        <BrowserHistory />
-        <Router>
           <Footer />
           <Switch>
+            <Route path="/" exact component={Home} />
             <Route path="/signup" exact component={About} />
             <Route path="/login" exact component={About} />
             <Route path="/about" exact component={About} />
             <Route path="/contact" exact component={About} />
           </Switch>
         </Router>
-      </React.Fragment>
+      </>
     );
   }
 }
