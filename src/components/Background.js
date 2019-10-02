6a3267d8c53bc98a8369a7562099292d9c468106
AndreAggalut <aaggalut3895@gmail.com>
2019-10-02 15:12:10 -0700

    background added

--- src/components/Background.js
+++ src/components/Background.js
@@ -8,6 +8,7 @@ export default class Background extends Component {
 
     this.state = {
       background: [],
+      maker: '',
     };
   }
 
@@ -15,16 +16,23 @@ export default class Background extends Component {
     const res = await axios.get(
       `https://flowhome-server.herokuapp.com/api/background`
     );
-
+    // this.style.background = `linear-gradient(rgba(36, 51, 91, 0.5), rgba(36, 51, 91, 0.5)), url('${res.data.urls.regular}')`;
+    // this.style.backgroundSize = `cover`;
+    // this.innerHTML = `${res.data.user.name}`;
+    // this.photoCredits.href = `${res.data.user.links.html}?utm_source=flow_homepage&utm_medium=referral`;
     this.setState({
-      background: res.data,
+      background: res.data.urls.regular,
+      maker: res.data.user.name,
+      photocredit: res.data.user.links.html,
     });
   }
   render() {
     return (
       <React.Fragment>
         <img src={this.state.background} alt="bg" className="bg" />
-        <div className="bg-color" />
+        <div>
+          {this.state.maker} {this.state.photocredit}
+        </div>
       </React.Fragment>
     );
   }
