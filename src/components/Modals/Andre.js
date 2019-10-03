53cab293cf60a96cdcb3e581a34486ab215e73b8
Dean Sumitro <DSumitro@talentpath.com>
2019-10-03 11:18:58 -0700

    unstyled modals

---
+++ src/components/Modals/Andre.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class Andre extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <span className="modal-exit">&times;</span>
+          <div>
+            <h1>Music Lover | Quote Appreciator</h1>
+            <ul>
+              <li>Interests, Curiosities, Motivations & Passions</li>
+              <li>Listening to music</li>
+              <li>Travel the world one place at a time</li>
+              <li>
+                Sports (lots of basketball and football, but anything really)
+              </li>
+              <li>
+                I enjoy learning new things everyday. Open to any discussions.
+              </li>
+              <li>Quirk: Daydreams a lot, but we still love them!</li>
+
+              <img src="/img/andre1.svg" alt="" />
+              <img src="/img/andre2.svg" alt="" />
+              <img src="/img/andre3.svg" alt="" />
+              <img src="/img/andre4.svg" alt="" />
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
