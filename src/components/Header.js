31791ec3bef8b329f34c263cee869f5b44163715
Dean Sumitro <dean.sumitro.27@gmail.com>
2019-10-31 10:22:59 -0700

    removed comment

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ export default class Header extends Component {
     return (
       <>
         <section className="logoWithText">
-          {/* point href to localhost when testing */}
           <a href="https://www.flowhome.us/">
             <img src="/img/flowlogo.png" alt="" className="logoImg" />
           </a>
