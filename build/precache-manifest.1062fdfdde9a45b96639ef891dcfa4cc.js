c644edf16775d341289b89b9a8580aedfa4ee2d6
Ivy <yunjilim@gmail.com>
2019-10-31 11:01:17 -0700

    build

---
+++ build/precache-manifest.1062fdfdde9a45b96639ef891dcfa4cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd41722fdb11badb56bcc255c1214f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "653a55f88f6bef62068b",
+    "url": "/static/css/main.a657d9b0.chunk.css"
+  },
+  {
+    "revision": "9cdc3f68264dded307e7",
+    "url": "/static/js/2.8764d4d7.chunk.js"
+  },
+  {
+    "revision": "653a55f88f6bef62068b",
+    "url": "/static/js/main.8706e16d.chunk.js"
+  },
+  {
+    "revision": "1deb5cbc6456952a69ca",
+    "url": "/static/js/runtime~main.abf17e30.js"
+  }
+]);
