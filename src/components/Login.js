351eb31698f59cd7aec76274651988a66ca5b598
Dean Sumitro <dean.sumitro.27@gmail.com>
2019-10-31 10:06:11 -0700

    missed some conflicts

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,6 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
-<<<<<<< HEAD
-  render() {
-    return (
-      <form>
-        <h3>Log In</h3>
-        <div class="group">
-          <input type="text" required />
-          <span className="highlight" />
-          <span className="bar" />
-          <label>Email</label>
-        </div>
-        <div class="group">
-          <input type="text" required />
-          <span className="highlight" />
-          <span className="bar" />
-          <label>Password</label>
-        </div>
-        <div className="buttons">
-          <button type="submit">Submit</button>
-          <button type="cancel">Cancel</button>
-        </div>
-      </form>
-    );
-  }
-=======
     render() {
         return (
             <form id ="signup-login">
@@ -49,5 +24,4 @@ export default class Login extends Component {
           </form>
         )
     }
->>>>>>> Staging
 }
